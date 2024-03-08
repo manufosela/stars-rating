aa2289079ded38c015205c05a5ce7c3b7f07813c
manufosela <manufosela@gmail.com>
2024-03-08 10:45:31 +0100

    fix: array stars initialization in constructor
    fix:radix parameter in parseInt
    
    chore:deleted o parameter in updated method

--- src/StarsRating.js
+++ src/StarsRating.js
@@ -35,59 +35,60 @@ export class StarsRating extends LitElement {
         type: Boolean,
         value: false,
       },
-    }
+    };
   }
 
   static get styles() {
     return css`
       :host(:not([hidden])) {
-          display: block;
-          font-size: 2em;
-          --star-size: 1em;
-          --star-color: #FFD700;
-          --star-unicode: '★';
-          --star-reset-unicode: 'ø';
-        }
-        fieldset,
-        label {
-          margin: 0;
-          padding: 0;
-        }
-        fieldset {
-          border: none;
-        }
-        input {
-          display: none;
-        }
-        label:before {
-          font-size: var(--star-size, 1em);
-          display: inline-block;
-          content: var(--star-unicode, "★");
-        }
-        label {
-          color: var(--star-color);
-          opacity: 0.3;
-        }
-        label[data-hightlight] {
-          opacity: 1;
-        }
-        label.resetbtn {
-          color: #F00;
-          font-size:0.5em;
-          opacity: 1;
-        }
-        label.resetbtn:before {
-          font-size: var(--star-size, 1em);
-          display: inline-block;
-          content: var(--star-reset-unicode, "ø");
-          opacity:1;
-        }
+        display: block;
+        font-size: 2em;
+        --star-size: 1em;
+        --star-color: #ffd700;
+        --star-unicode: '★';
+        --star-reset-unicode: 'ø';
+      }
+      fieldset,
+      label {
+        margin: 0;
+        padding: 0;
+      }
+      fieldset {
+        border: none;
+      }
+      input {
+        display: none;
+      }
+      label:before {
+        font-size: var(--star-size, 1em);
+        display: inline-block;
+        content: var(--star-unicode, '★');
+      }
+      label {
+        color: var(--star-color);
+        opacity: 0.3;
+      }
+      label[data-hightlight] {
+        opacity: 1;
+      }
+      label.resetbtn {
+        color: #f00;
+        font-size: 0.5em;
+        opacity: 1;
+      }
+      label.resetbtn:before {
+        font-size: var(--star-size, 1em);
+        display: inline-block;
+        content: var(--star-reset-unicode, 'ø');
+        opacity: 1;
+      }
     `;
   }
 
   constructor() {
     super();
     this._rate = this._rate.bind(this);
+    this._stars = [];
   }
 
   connectedCallback() {
@@ -97,14 +98,16 @@ export class StarsRating extends LitElement {
 
   disconnectedCallback() {
     if (super.disconnectedCallback) super.disconnectedCallback();
-    this.renderRoot.querySelector('#rating').removeEventListener('click', this._rate);
+    this.renderRoot
+      .querySelector('#rating')
+      .removeEventListener('click', this._rate);
   }
 
-  updated(changedProperties, o) {
+  updated(changedProperties) {
     if (changedProperties.get('manual') !== this.manual) {
       this._manualChanged();
     }
-    if (changedProperties.get('rating') !==  this.rating) {
+    if (changedProperties.get('rating') !== this.rating) {
       this._ratingChange();
     }
   }
@@ -123,7 +126,9 @@ export class StarsRating extends LitElement {
     } else if (this.rating > this.numstars) {
       this.rating = this.numstars;
     }
-    this.dispatchEvent(new CustomEvent('rating-changed', { detail: this.rating }));
+    this.dispatchEvent(
+      new CustomEvent('rating-changed', { detail: this.rating })
+    );
   }
 
   reset() {
@@ -132,28 +137,49 @@ export class StarsRating extends LitElement {
 
   _manualChanged() {
     if (this.manual) {
-      this.renderRoot.querySelector('#rating').addEventListener('click', this._rate);
+      this.renderRoot
+        .querySelector('#rating')
+        .addEventListener('click', this._rate);
     } else {
-      this.renderRoot.querySelector('#rating').removeEventListener('click', this._rate);
+      this.renderRoot
+        .querySelector('#rating')
+        .removeEventListener('click', this._rate);
     }
   }
 
   _rate(ev) {
     if (ev.target.nodeName === 'INPUT') {
-      this.rating = parseInt(ev.target.value) + 1;
+      this.rating = parseInt(ev.target.value, 10) + 1;
     }
   }
 
   render() {
     return html`
       <fieldset id="rating">
-        ${(this.resetbtn && this.manual) ? html`<label class="resetbtn"><input @click="${this.reset}" type="radio" id="x" name="resetbtn" value="-1" /></label>  ` : html``}
+        ${this.resetbtn && this.manual
+          ? html`<label class="resetbtn"
+              ><input
+                @click="${this.reset}"
+                type="radio"
+                id="x"
+                name="resetbtn"
+                value="-1"
+            /></label> `
+          : html``}
         ${repeat(
           this._stars,
           item => item,
-          (item, index) => html`<label for="star${index}" ?data-hightlight="${this._isHightlight(index)}">
-                              <input type="radio" id="star${index}" name="rating" .value="${index.toString()}" />
-                            </label>`
+          (item, index) => html`<label
+            for="star${index}"
+            ?data-hightlight="${this._isHightlight(index)}"
+          >
+            <input
+              type="radio"
+              id="star${index}"
+              name="rating"
+              .value="${index.toString()}"
+            />
+          </label>`
         )}
       </fieldset>
     `;
